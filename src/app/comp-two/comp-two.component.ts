b467fdcb00a73b26cf91f63cca8e942b5d3219d0
Sherry Xiong <xsherry22@gmail.com>
2020-04-05 21:50:12 -0400

    add animations

--- src/app/comp-two/comp-two.component.ts
+++ src/app/comp-two/comp-two.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger, 
+  state, 
+  style,
+  animate, 
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-comp-two',
   templateUrl: './comp-two.component.html',
-  styleUrls: ['./comp-two.component.css']
+  styleUrls: ['./comp-two.component.css'],
+  animations: [
+    trigger('onOff', [
+      state('on', 
+      style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'orange'
+      })),
+      state('off', 
+      style({
+        height: '200px',
+        opacity: 0.5,
+        backgroundColor: 'grey'
+      })),
+      transition('on => off', [
+        animate('0.3s')
+      ]),
+      transition('off => on', [
+        animate('1s')
+      ]),
+    ]),
+  ],
 })
 export class CompTwoComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  isOn = true;
+
+  toggle(){
+    this.isOn = !this.isOn;
   }
 
 }
